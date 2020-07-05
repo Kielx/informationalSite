2240059bba200049d96b43d72e7940b4bd349eb5
Kielx <kiel66@gmail.com>
2020-07-05 21:19:44 +0200

    Add server logic

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var http = require("http");
 var fs = require("fs");
+const url = require("url");
 
 server = http.createServer(function (req, res) {
-  fs.readFile("index.html", function (err, data) {
+  let q = url.parse(req.url, true);
+  let filename = "." + q.pathname;
+  fs.readFile(filename, function (err, data) {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      return res.end("404 Not Found");
+    }
     res.writeHead(200, { "Content-Type": "text/html" });
     res.write(data);
     console.log("running");
